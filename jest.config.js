5763354cd25d7ec817bef1ff25fe91051f6624e9
gaosheng08 <gaosheng08@meituan.com>
2022-03-03 18:09:23 +0800

    更新了 0.0.15 版本。

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(lodash-es|other-es-lib))'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(lodash-es|dayjs|other-es-lib))'],
     testPathIgnorePatterns: ['<rootDir>/src/browser.ts']
 }
--- src/object.ts
+++ src/object.ts
@@ -1,38 +1,42 @@
-import { camelCase, snakeCase, isPlainObject } from 'lodash-es';
+import { camelCase, snakeCase, isPlainObject, isArray } from 'lodash-es';
 
 const recursionObjectKey: (fn: Function) => (obj: object) => object = (fn: Function) => (obj: object) => {
     const set = new Set();
 
     set.add(obj);
 
-    return Object.fromEntries(Object.entries(obj).map(([key, value]) => [
-        fn(key),
-        isPlainObject((value)) && !set.has(value) ? recursionObjectKey(fn)(value) : value
-    ]));
+    return isArray(obj)
+        ? (obj as any[]).map(recursionObjectKey(fn))
+        : Object.fromEntries(Object.entries(obj).map(([key, value]) => [
+            fn(key),
+            isPlainObject((value)) && !set.has(value) ? recursionObjectKey(fn)(value) : value
+        ]));
 };
 
 /**
- * 将对象中的所有键驼峰化，支持循环引用检查，用于前后端交互（以及后端与数据库交互）时的数据格式转换
+ * 将对象中的所有键驼峰化，支持循环引用检查，用于前后端交互（以及后端与数据库交互）时的数据格式转换，支持对象数组
  *
  * @param obj 被转换的对象
  *
  * @example
  *
  * ```typescript
- * camelCaseObject({ 'a-b': { c_d: 1 } })   // { aB: { cD: 1 } }
+ * camelCaseObject({ 'a-b': { c_d: 1 } })       // { aB: { cD: 1 } }
+ * camelCaseObject([{ a_b: 1 }, { c_d: 2 }])    // [{ aB: 1 }, { cD: 2 }]
  * ```
  */
 export const camelCaseObject = recursionObjectKey(camelCase);
 
 /**
- * 将对象中的所有键下划线化，支持循环引用检查，用于前后端交互（以及后端与数据库交互）时的数据格式转换
+ * 将对象中的所有键下划线化，支持循环引用检查，用于前后端交互（以及后端与数据库交互）时的数据格式转换，支持对象数组
  *
  * @param obj 被转换的对象
  *
  * @example
  *
  * ```typescript
- * snakeCaseObject({ aB: { cD: 1 } })   // { a_b: { c_d: 1 } }
+ * snakeCaseObject({ aB: { cD: 1 } })           // { a_b: { c_d: 1 } }
+ * snakeCaseObject([{ aB: 1 }, { cD: 2 }])      // [{ a_b: 1 }, { c_d: 2 }]
  * ```
  */
 export const snakeCaseObject = recursionObjectKey(snakeCase);
