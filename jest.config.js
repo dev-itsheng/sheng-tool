d90ee4202790909e1ce26107f04b1b42db2b91fc
Gao Sheng <gaosheng08@meituan.com>
2022-08-11 11:25:23 +0800

    调整一个细节

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // 由于 pnpm 使用符号链接，因此需要加一个 .pnpm 路径
-    transformIgnorePatterns: ['<rootDir>/node_modules/.pnpm/(?!(lodash-es|dayjs|other-es-lib))']
+    transformIgnorePatterns: ['<rootDir>/node_modules/.pnpm/(?!(lodash-es|dayjs|other-es-lib)@)']
 };
